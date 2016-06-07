328d9c1110edc93fef50f723fc1e2440f5300c70
PabloRivadavia <pablo.rivadavia98@gmail.com>
2016-06-07 20:04:41 -0300

    flying hero parte 1

---
+++ flying_hero/game.rb
@@ -0,0 +1,20 @@
+require 'gosu'
+class Game < Gosu::Window
+def initialize
+	super(900, 550, fullscreen: false)
+	self.caption = 'Flying hero!'
+end
+
+	def draw
+	end
+
+	def button_down (id)
+		if id == Gosu::KbEscape
+			close
+		end
+	end
+
+
+
+end
+
