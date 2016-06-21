1a0f9eb4158425d6b119cc839fa6d7b2f0603793
PabloRivadavia <pablo.rivadavia98@gmail.com>
2016-06-21 18:52:23 -0300

    Background y heroe inmovil

--- flying_hero/game.rb
+++ flying_hero/game.rb
@@ -1,11 +1,18 @@
 require 'gosu'
+require_relative 'background'
+require_relative 'hero'
 class Game < Gosu::Window
 def initialize
 	super(900, 550, fullscreen: false)
 	self.caption = 'Flying hero!'
+	@background = Background.new
+	@hero = Hero.new
+
 end
 
 	def draw
+		@background.draw
+		@hero.draw
 	end
 
 	def button_down (id)
@@ -14,6 +21,9 @@ end
 		end
 	end
 
+	def update
+		@background.scroll!
+	end
 
 
 end
---
+++ flying_hero/hero.rb
@@ -0,0 +1,16 @@
+require 'gosu'
+class Hero
+
+  def initialize
+    @sprites = Gosu::Image::load_tiles('assets/images/cat.png', 847/6, 87, tileable: false)
+    @width = @sprites.first.width
+    @height = @sprites.first.height
+    @y = 250
+    @x = 10
+  end
+
+  def draw
+    sprite = @sprites[Gosu::milliseconds / 75% @sprites.size]
+    sprite.draw(@x, @y, 1)
+  end
+end
