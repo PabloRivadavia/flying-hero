49ccf20f3d90763baec8819788f66e6495073ccc
PabloRivadavia <pablo.rivadavia98@gmail.com>
2016-06-21 19:54:24 -0300

    Candy

---
+++ flying_hero/candy.rb
@@ -0,0 +1,8 @@
+require 'gosu'
+require_relative 'flying_item'
+class Candy < FlyingItem
+
+def self.image_path
+  'assets/images/candy.png'
+end
+end
--- flying_hero/game.rb
+++ flying_hero/game.rb
@@ -1,18 +1,21 @@
 require 'gosu'
 require_relative 'background'
 require_relative 'hero'
+require_relative 'candy'
 class Game < Gosu::Window
 def initialize
 	super(900, 550, fullscreen: false)
 	self.caption = 'Flying hero!'
 	@background = Background.new
 	@hero = Hero.new
+	@candy = Candy.new(self)
 
 end
 
 	def draw
 		@background.draw
 		@hero.draw
+		@candy.draw
 	end
 
 	def button_down (id)
